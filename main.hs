988ef00bd82ec2e9af76e03ca534d40c077ca40f
lazzlo2096 <lazzlo2096@yandex.ru>
2018-08-01 16:24:09 +0300

    Such OOP-style :\;

--- main.hs
+++ main.hs
@@ -1,21 +1,27 @@
 -- BF interpreter
 -- main = putStr "Hello!"
--- main = interpretBF (parPar input) [0] 0
+-- main = interpretBF (parseBF input) [0] 0
 
 input = "+-++." -- [2]
 
 data BFterm = 
   Add |
   Sub |
+  Next |
+  Back |
+  Read |
   Print
   deriving(Show, Eq)
 
-parPar a = map parseBFterms a
+parseBF a = map parseBFterms a
 
 parseBFterms c
   | c=='+' = Add
   | c=='-' = Sub
   | c=='.' = Print
+  | c=='>' = Next
+  | c=='<' = Back
+  | c==',' = Read
   
 
 myMapElement :: [t] -> Int -> (t -> t) -> [t] --Автовыведено
@@ -30,7 +36,28 @@ interpretBF (x:xs) memSpace pointer
   | x==Add = interpretBF xs ( myMapElement memSpace pointer (+1) ) pointer
   | x==Sub = interpretBF xs ( myMapElement memSpace pointer (\x -> x-1) ) pointer
   | x==Print = interpretBF xs ( myMapElement memSpace pointer id ) pointer  --тут должен делать побочный эффект, кек -- пока просто пропускает
+ 
+interpretBF' :: Enum a => [BFterm] -> [a] -> Int -> [a] -- Ещё раз Автовыведено
+interpretBF' (x:xs) memSpace pointer = 
+  interpretBF' xs ( myMapElement memSpace pointer (getFunc4Value x) ) ((getFunc4Pointer x) pointer)
+interpretBF' [] memSpace pointer = memSpace 
+
+getFunc4Value x
+  | x==Add = succ
+  | x==Sub = pred
+  -- | x==Print = id
+  | otherwise = id
+
+getFunc4Pointer x
+  | x==Next = succ
+  | x==Back = pred
+  | otherwise = id
   
+-- getFunc4memSpace x
+  -- | x==Next = ...
+  -- | x==Back = ...
+  -- | otherwise = ...
+ 
 testt [] = 5
 testt (x:[]) = x
 testt (x:xs) = testt xs
