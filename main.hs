98aa31cb0f60e0f9523b1c1317e4cd2f837607b7
lazzlo2096 <lazzlo2096@yandex.ru>
2018-08-02 11:11:47 +0300

    Yeah, babe\!;

--- main.hs
+++ main.hs
@@ -1,8 +1,11 @@
 -- BF interpreter
 
--- main = interpretBF (parseBF input) ([],[])
+import Data.Char -- ord chr
 
-input = "+-++." -- [2]
+main = interpretBF (parseBF input2) ([],[]) []
+
+input = "+-++."
+input2 = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+++++++++++++++++++++++++++++.+++++++..+++.-------------------------------------------------------------------------------.+++++++++++++++++++++++++++++++++++++++++++++++++++++++.++++++++++++++++++++++++.+++.------.--------.-------------------------------------------------------------------.-----------------------."
 
 data BFterm = 
   Increment |
@@ -24,20 +27,27 @@ parseBFterms c
   | c==',' = Read
   
 -- data MemSpace a = YYY ([a],[a]) -- world
+showMemSpace (x, y) = map chr (x++y)
 
-interpretBF :: (Num a, Enum a) => [BFterm] -> ([a], [a]) -> ([a], [a]) -- Автовыведено
-interpretBF (x:xs) memSpace = interpretBF xs $ change x memSpace
-interpretBF [] memSpace = memSpace 
+interpretBF :: [BFterm] -> ([Int], [Int]) -> [IO ()] -> IO [()] -- Автовыведено -- кек, IO [()] - шо это ?
+interpretBF (x:xs) memSpace aa = let (a,b) = change x memSpace in interpretBF xs a (aa++b)
+interpretBF [] memSpace aa = sequence aa
 
 -- apply3TupleToFunc f (x,y,z) = f x y z -- Есть ли в Хаскеле такая стандартная функция?
 
--- вместо пары pointer и [] можно юзать просто ( (xss:xs) , (z:zs)) где z - будет элемент подуказателем
+-- вместо пары pointer и [] можно юзать просто (zs, (x:xs)) где x - будет элемент подуказателем
 -- подсматрел это у https://github.com/jrp2014/BF/blob/master/src/BF.hs#L65
 change term (zs, []) = change term (zs, [0])
 change term ([], (x:xs)) = change term ([0], (x:xs)) -- почему только с этой строкой выводиться варнинг если её переместить после определения change ?
 change term (zs, (x:xs))
-  | term==Increment = ( zs, succ x :xs )
-  | term==Decrement = ( zs, pred x :xs )
-  | term==Forward = ( zs++[x], xs )
-  | term==Back = ( init zs, last zs :x:xs )
-  | otherwise = (zs, (x:xs)) -- do nothing
+  | term==Increment = (( zs, succ x :xs ), [])
+  | term==Decrement = (( zs, pred x :xs ), [])
+  | term==Forward = (( zs++[x], xs ), [])
+  | term==Back = (( init zs, last zs :x:xs ), [])
+  | term==Print = ((zs, (x:xs)), [do putStr [chr x]])
+  | otherwise = ((zs, (x:xs)), []) -- do nothing
+
+
+g  = ([72,101,108],[108,111,33]) :: ([Int],[Int])
+f g = do
+  putStrLn $ showMemSpace g
